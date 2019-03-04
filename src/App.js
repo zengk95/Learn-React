4c86981f6a7cd5b118a95c34da573da2067f9805
Kerry <zengk95@gmail.com>
2019-03-04 13:11:02 -0600

    added search funcitnoality to page

--- src/App.js
+++ src/App.js
@@ -2,77 +2,82 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
 
 
-const users = { a: "Robin", b: 'Test', c: 'FAS' };
+const DEFAULT_QUERY = 'redux';
 
-const { a, test2, test3 } = users;
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
-console.log(a, test2, test3);
-
-
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const urlPath = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}`;
+const url = `${urlPath}${DEFAULT_QUERY}`;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+    console.log(this.state);
+  }
+
+  onSearchSubmit = (event) => {
+    const { searchTerm } = this.state;
+    console.log(searchTerm);
+    this.fetchTopStories(searchTerm);
+    event.preventDefault();
   }
 
   onDismiss = (id) => {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    console.log(updatedList)
-    this.setState({ list: updatedList });
+    const updatedList = this.state.result.hits.filter(isNotId);
+    this.setState({ result: { ...this.state.result, hits: updatedList } });
   }
 
   onSearchChange = (event) => {
     this.setState({ searchTerm: event.target.value });
-    console.log(event.target.value);
-
   }
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchTopStories(searchTerm);
+  }
 
+  fetchTopStories = (searchTerm) => {
+    fetch(`${urlPath}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .then(error => error);
+  }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    console.log(result);
     return (
       <div className="App">
         <Search
           value={searchTerm}
-          onChange={this.onSearchChange}>
+          onChange={this.onSearchChange}
+          onSubmit={this.onSearchSubmit}>
           <div>
             Search
           </div>
-          <span>Hello</span>
         </Search>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss} />
-
+        {
+          result ?
+            <Table
+              result={result}
+              onDismiss={this.onDismiss} />
+            : null
+        }
       </div>
     );
   }
@@ -80,15 +85,16 @@ class App extends Component {
 
 class Search extends Component {
   render() {
-    const { value, onChange, children } = this.props;
-    console.log(children);
+    const { value, onChange, onSubmit, children } = this.props;
     return (
-      <form>
-        {children}
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           value={value}
           onChange={onChange} />
+        <button type="submit">
+          {children}
+        </button>
       </form>
     );
   }
@@ -96,10 +102,11 @@ class Search extends Component {
 
 class Table extends Component {
   render() {
-    const { list, pattern, onDismiss } = this.props;
+    const { result, onDismiss } = this.props;
+    console.log(result);
     return (
       <div>
-        {list.filter(isSearched(pattern)).map(item =>
+        {result.hits.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
